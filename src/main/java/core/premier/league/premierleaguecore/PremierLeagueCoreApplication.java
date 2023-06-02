a20c7e0093a72d6f9df125f1a3b4e1242a890dda
Ruwan <ruwanliyanagegalle1994@gmail.com>
2023-06-02 22:22:35 +0530

    Add init commit

---
+++ src/main/java/core/premier/league/premierleaguecore/PremierLeagueCoreApplication.java
@@ -0,0 +1,13 @@
+package core.premier.league.premierleaguecore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremierLeagueCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PremierLeagueCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/core/premier/league/premierleaguecore/PremierLeagueCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package core.premier.league.premierleaguecore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremierLeagueCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
