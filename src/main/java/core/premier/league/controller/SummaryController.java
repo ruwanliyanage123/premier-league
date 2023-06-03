9d8a96b3a84db8082f647de1c1fc0b5f9f55f5a0
Ruwan <ruwanliyanagegalle1994@gmail.com>
2023-06-03 22:07:25 +0530

    Add Cross Origin

--- src/main/java/core/premier/league/controller/SummaryController.java
+++ src/main/java/core/premier/league/controller/SummaryController.java
@@ -7,6 +7,7 @@ import core.premier.league.service.SummaryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/summary")
+@CrossOrigin(origins = "http://localhost:4200")
 public class SummaryController {
     @Autowired
     private SummaryService summaryService;
